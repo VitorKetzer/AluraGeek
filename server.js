6c2f0f3af724343202c51f47c2734efaa9ac144d
VitorKetzer <163921807+VitorKetzer@users.noreply.github.com>
2024-07-07 00:49:39 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,15 @@ const server = express();
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
 
+server.use(express.static(__dirname));
+
+server.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 server.use(middlewares);
-server.use(router);
+
+server.use('/api', router);
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
